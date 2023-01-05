d607b794276108e1b8f45a1c401ba0ff6c7eca19
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2023-01-05 18:32:27 +0530

    initial commit

---
+++ AzureIOT/Controllers/DeviceController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AzureIOT.Repositories;
+
+namespace AzureIOT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeviceController : ControllerBase
+    {
+        [HttpPost("AddDevice")]
+        public async Task<string> AddDevice(string deviceName)
+        {
+            await DeviceRepository.AddDeviceAsync(deviceName);
+            return null;
+        }
+    }
+}
---
+++ AzureIOT/Repositories/DeviceRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+using Microsoft.Azure.Devices.Common.Exceptions;
+
+namespace AzureIOT.Repositories
+{
+    public class DeviceRepository
+    {
+        public static RegistryManager registryManager;
+        private static string connStringIotHub = "";
+        public static async Task AddDeviceAsync(string deviceName)
+        {
+            if (string.IsNullOrEmpty(deviceName))
+            {
+                throw new ArgumentNullException("noDeviceName");
+            }
+
+            Device device;
+            registryManager = RegistryManager.CreateFromConnectionString(connStringIotHub);
+            device = await registryManager.AddDeviceAsync(new Device(deviceName));
+            return;
+        }
+    }
+}
