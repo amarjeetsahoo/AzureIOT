8591dae0edaee576b9f35a8ed117e192c68d1e1b
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2023-01-06 12:35:38 +0530

    added: deviceController

--- AzureIOT/Controllers/DeviceController.cs
+++ AzureIOT/Controllers/DeviceController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using AzureIOT.Repositories;
+using Microsoft.Azure.Devices;
 
 namespace AzureIOT.Controllers
 {
@@ -13,10 +14,33 @@ namespace AzureIOT.Controllers
     public class DeviceController : ControllerBase
     {
         [HttpPost("AddDevice")]
-        public async Task<string> AddDevice(string deviceName)
+        public async Task AddDevice(string deviceId)
         {
-            await DeviceRepository.AddDeviceAsync(deviceName);
-            return null;
+            await DeviceRepository.AddDeviceAsync(deviceId);
+            return;
+        }
+
+        [HttpGet("GetDevice")]
+        public async Task<Device> GetDevice(string deviceId)
+        {
+            Device device;
+            device=await DeviceRepository.GetDeviceAsync(deviceId);
+            return device;
+        }
+
+        [HttpDelete("DeleteDevice")]
+        public async Task DeleteDevice(string deviceId)
+        {
+            await DeviceRepository.DeleteDeviceAsync(deviceId);
+            return;
+        }
+
+        [HttpPut("UpdateDevice")]
+        public async Task<Device> UpdateDevice(string deviceId)
+        {
+            Device device;
+            device= await DeviceRepository.UpdateDeviceAsync(deviceId);
+            return device;
         }
     }
 }
