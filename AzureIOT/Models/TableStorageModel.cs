6bb770dfd895a10a92c8d02e5476fded738d4e22
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2023-01-11 15:04:31 +0530

    updated: DevicePropertiesModel

---
+++ AzureIOT/Models/TableStorageModel.cs
@@ -0,0 +1,17 @@
+﻿using Azure;
+using Azure.Data.Tables;
+using System;
+
+namespace AzureIOT.Models
+{
+    public class TableStorageModel: ITableEntity
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Department { get; set; }
+        public string PartitionKey { get; set; }
+        public string RowKey { get; set; }
+        public DateTimeOffset? Timestamp { get; set; }
+        public ETag ETag { get; set; }
+    }
+}
