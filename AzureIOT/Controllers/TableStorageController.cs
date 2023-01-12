af83b1232533cc2ac619fe950a2fb459a0b8778c
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2023-01-12 15:43:00 +0530

    added: Table Storage

---
+++ AzureIOT/Controllers/TableStorageController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AzureIOT.Repositories;
+using Azure.Data.Tables;
+using AzureIOT.Models;
+
+namespace AzureIOT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TableStorageController : ControllerBase
+    {
+        [HttpPost("AddTable")]
+        public async Task AddTable(string tableName)
+        {
+            await TableStorageRepository.AddTable(tableName);
+            return;
+        }
+
+        [HttpPut("UpdateTable")]
+        public async Task<TableStorageModel> Updatetable(TableStorageModel employee, string tableName)
+        {
+            employee.PartitionKey = tableName;
+            string Id = Guid.NewGuid().ToString();
+            employee.Id = Id;
+            employee.RowKey = Id;
+            employee.Timestamp = DateTime.Now;
+            var data = await TableStorageRepository.Updatetable(employee, tableName);
+            return data;
+        }
+
+        [HttpGet("GetTableData")]
+        public async Task<TableStorageModel> GetTableData(string tableName, string partitionKey, string id)
+        {
+            var data = await TableStorageRepository.GetTableData(tableName, partitionKey, id);
+            return data;
+        }
+
+        [HttpGet("GetTable")]
+        public TableClient GetTable(string tableName)
+        {
+            var data = TableStorageRepository.GetTable(tableName);
+            return data;
+        }
+
+        [HttpGet("GetAllTableData")]
+        public async Task<IEnumerable<TableStorageModel>> GetAllTableData(string tableName)
+        {
+            var data = await TableStorageRepository.GetAllTableData(tableName);
+            return data;
+        }
+
+        [HttpDelete("DeleteTableData")]
+        public async Task DeleteTableData(string tableName, string partitionKey, string id)
+        {
+            await TableStorageRepository.DeleteTableData(tableName, partitionKey, id);
+            return;
+        }
+
+        [HttpDelete("DeleteTable")]
+        public async Task DeleteTable(string tableName  )
+        {
+            await TableStorageRepository.DeleteTable(tableName);
+            return;
+        }
+    }
+}
---
+++ AzureIOT/Repositories/TableStorageRepository.cs
@@ -0,0 +1,70 @@
+﻿using Azure.Data.Tables;
+using AzureIOT.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AzureIOT.Repositories
+{
+    public class TableStorageRepository
+    {
+        private static string connStringStorage = "DefaultEndpointsProtocol=https;AccountName=storageahs230112;AccountKey=GQBehDz8SzJuUwzzzeabojmVk7qN2U6aQXPrHgh4Msc6FhCjCQPJ3VIjjNKvAgClU7mJ01rMeXHS+ASt2iVZow==;EndpointSuffix=core.windows.net";
+
+        public static async Task AddTable(string tableName)
+        {
+            var data = new TableServiceClient(connStringStorage);
+            var client = data.GetTableClient(tableName);
+            await client.CreateIfNotExistsAsync();
+        }
+
+        public static async Task<TableStorageModel> Updatetable(TableStorageModel employee, string tableName)
+        {
+            var data = new TableServiceClient(connStringStorage);
+            var client = data.GetTableClient(tableName);
+            await client.UpsertEntityAsync(employee);
+            return employee;
+        }
+     
+        public static async Task<TableStorageModel> GetTableData(string tableName, string partitionKey, string id)
+        {
+            var data = new TableServiceClient(connStringStorage);
+            var client = data.GetTableClient(tableName);
+            var tableData = await client.GetEntityAsync<TableStorageModel>(partitionKey, id);
+            return tableData;
+        }
+
+        public static TableClient GetTable(string tableName)
+        {
+            var data = new TableServiceClient(connStringStorage);
+            var client = data.GetTableClient(tableName);
+            return client;
+        }
+
+        public static async Task<IEnumerable<TableStorageModel>> GetAllTableData(string tableName)
+        {
+            var _tableClient = GetTable(tableName);
+            IList<TableStorageModel> modelList = new List<TableStorageModel>();
+            var data = _tableClient.QueryAsync<TableStorageModel>(filter: "", maxPerPage: 10);
+            await foreach (var rec in data)
+            {
+                modelList.Add(rec);
+            }
+            return modelList;
+        }
+
+            public static async Task DeleteTableData(string tableName, string partitionKey, string id)
+        {
+            var data = new TableServiceClient(connStringStorage);
+            var client = data.GetTableClient(tableName);
+            await client.DeleteEntityAsync(partitionKey, id);
+            return;
+        }
+
+        public static async Task DeleteTable(string tableName)
+        {
+            var data = new TableServiceClient(connStringStorage);
+            var client = data.GetTableClient(tableName);
+            await client.DeleteAsync();
+            return;
+        }
+    }
+}
