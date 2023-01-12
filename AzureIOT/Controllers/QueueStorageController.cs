e610e9483599141dadcffc5dbf3667c64f3ec87f
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2023-01-12 15:42:37 +0530

    added: Queue Storage

---
+++ AzureIOT/Controllers/QueueStorageController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using AzureIOT.Repositories;
+using Azure.Storage.Queues.Models;
+
+namespace AzureIOT.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueueStorageController : ControllerBase
+    {
+        [HttpPost("AddQueue")]
+        public async Task<string> AddQueue(string queueName)
+        {
+            await QueueStorageRepository.CreateQueue(queueName);
+            return null;
+        }
+
+        [HttpPost("InsertMessage")]
+        public async Task<string> InsertMessage(string queueName, string msg)
+        {
+            await QueueStorageRepository.InsertMessage(queueName, msg);
+            return null;
+        }
+
+        [HttpGet("PeekMessage")]
+        public PeekedMessage[] PeekMessage(string queueName, int peekValue)
+        {
+            var data = QueueStorageRepository.PeekMessage(queueName, peekValue);
+            return data;
+        }
+
+        [HttpPut("UpdateMessage")]
+        public void UpdateMessage(string queueName, string updatedMsg)
+        {
+            QueueStorageRepository.UpdateMessage(queueName, updatedMsg);
+            return;
+        }
+
+        [HttpPut("DequeueMessage")]
+        public void DequeueMessage(string queueName)
+        {
+            QueueStorageRepository.DequeueMessage(queueName);
+            return;
+        }
+
+        [HttpDelete("DeleteQueue")]
+        public async Task<string> DeleteQueue(string queueName)
+        {
+            await QueueStorageRepository.DeleteQueue(queueName);
+            return null;
+        }
+    }
+}
---
+++ AzureIOT/Repositories/QueueStorageRepository.cs
@@ -0,0 +1,98 @@
+﻿using Azure.Storage.Queues;
+using Azure.Storage.Queues.Models;
+using System;
+using System.Threading.Tasks;
+
+namespace AzureIOT.Repositories
+{
+    public class QueueStorageRepository
+    {
+        private static string connStringStorage = "DefaultEndpointsProtocol=https;AccountName=storageahs230112;AccountKey=GQBehDz8SzJuUwzzzeabojmVk7qN2U6aQXPrHgh4Msc6FhCjCQPJ3VIjjNKvAgClU7mJ01rMeXHS+ASt2iVZow==;EndpointSuffix=core.windows.net";
+
+        public static async Task<bool> CreateQueue(string queueName)
+        {
+            if (string.IsNullOrEmpty(queueName))
+            {
+                throw new ArgumentNullException("Queue Name Missing!");
+            }
+            try
+            {
+                QueueClient queue = new QueueClient(connStringStorage, queueName);
+                await queue.CreateIfNotExistsAsync();
+                if (queue.Exists())
+                {
+                    Console.WriteLine("Queue Created:" + queue.Name);
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Check Azure Emulator connection and try again");
+                    return false;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public static async Task<string> InsertMessage(string queueName, string msg)
+        {
+            if (string.IsNullOrEmpty(queueName) || string.IsNullOrEmpty(msg))
+            {
+                throw new ArgumentNullException("Parameter Missing!");
+            }
+
+            QueueClient queue = new QueueClient(connStringStorage, queueName);
+            await queue.CreateIfNotExistsAsync();
+            if (queue.Exists())
+            {
+                var data = queue.SendMessage(msg);
+                Console.WriteLine($"Inserted: {msg}");
+            }
+            return null;
+        }
+
+        public static PeekedMessage[] PeekMessage(string queueName, int peekValue)
+        {
+            QueueClient queue = new QueueClient(connStringStorage, queueName);
+            PeekedMessage[] msg = null;
+            if (queue.Exists())
+            {
+                msg = queue.PeekMessages(peekValue);
+            }
+            return msg;
+        }
+
+        public static void UpdateMessage(string queueName, string updatedMsg)
+        {
+            QueueClient queue = new QueueClient(connStringStorage, queueName);
+            if (queue.Exists())
+            {
+                QueueMessage[] msg = queue.ReceiveMessages();
+                queue.UpdateMessage(msg[0].MessageId, msg[0].PopReceipt,
+                    updatedMsg, TimeSpan.FromSeconds(60.0));
+            }
+        }
+
+        public static void DequeueMessage(string queueName)
+        {
+            QueueClient queue = new QueueClient(connStringStorage, queueName);
+            if (queue.Exists())
+            {
+                QueueMessage[] msg = queue.ReceiveMessages();
+                System.Console.WriteLine($"Dequeued message: '{msg[0].Body}'");
+                queue.DeleteMessage(msg[0].MessageId, msg[0].PopReceipt);
+            }
+        }
+
+        public static async Task DeleteQueue(string queueName)
+        {
+            QueueClient queue = new QueueClient(connStringStorage, queueName);
+            if (queue.Exists())
+            {
+                await queue.DeleteAsync();
+            }
+        }
+    }
+}
