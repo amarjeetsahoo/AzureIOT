acf6a16d9277a28e00c94d6227f66cd9e4a464e1
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2023-01-16 17:10:36 +0530

    updated: HomeRoute

--- AzureIOT/Controllers/HomeController.cs
+++ AzureIOT/Controllers/HomeController.cs
@@ -10,7 +10,7 @@ namespace AzureIOT.Controllers
         [HttpGet]
         public ActionResult Get()
         {
-            var path = "www/index.html";
+            var path = "wwwroot/index.html";
             StreamReader reader = new StreamReader(path);
             var fileBytes = System.IO.File.ReadAllBytes(path);
             FileContentResult file = File(fileBytes, "text/html");
